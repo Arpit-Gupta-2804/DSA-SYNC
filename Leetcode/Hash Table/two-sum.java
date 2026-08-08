8dde440b1461c0d3c23248c6b80c4e5790d42b37
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-08-08 23:08:25 +0530

    Solved Two Sum

--- Leetcode/Hash Table/two-sum.java
+++ Leetcode/Hash Table/two-sum.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      Hashtable<Integer, Integer> hashTable = new Hashtable<Integer, Integer>();
-      int i = 0;
-      while ((i < nums.length) && (hashTable.get(nums[i]) == null)) {      
-        hashTable.put(target - nums[i], i);
-        i++;
-      }
-      if (i < nums.length) {
-        return new int[]{hashTable.get(nums[i]),i};
-      } 
-      return null;       
+        int [] arr = {-1,-1};
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i < nums.length; i++){
+            int rem = target - nums[i];
+            if(hm.containsKey(rem)){
+                arr[0] = hm.get(rem);
+                arr[1] = i;
+            }
+            hm.put(nums[i], i);
+        }
+        return arr;
     }
 }
