9ee1e71d6525c7205887086f1345778feb6b3bbe
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-08-13 18:01:56 +0530

    Solved Topological Sort

--- GFG/Graph/topological-sort.java
+++ GFG/Graph/topological-sort.java
@@ -1,1 +1,23 @@
-[object Object]
+class Solution {
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int [] indegree = new int [V];
+        
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            indegree[v]++;
+            adj.get(u).add(v);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<indegree.length; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
