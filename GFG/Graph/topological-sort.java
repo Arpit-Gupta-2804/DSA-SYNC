49611f3b78e3b030c687dbfff5c8bf33048c7844
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-08-13 17:36:13 +0530

    Solved Topological Sort

--- GFG/Graph/topological-sort.java
+++ GFG/Graph/topological-sort.java
@@ -1,32 +1,22 @@
-// DFS Solution
-class Solution
-{
-	public:
-	
-	void dfs(int start, vector<bool> &vis, stack<int> &s, vector<int> adj[]) {
-	    vis[start] = 1;
-	    
-	    for(int nbr : adj[start]) {
-	        if(!vis[nbr]) {
-	            dfs(nbr, vis, s, adj);
-	        }
-	    }
-	    
-	    s.push(start);
-	}
-	
-	vector<int> topoSort(int V, vector<int> adj[]) {
-	    vector<bool> vis(V);
-	    stack<int> s;
-	    for(int i = 0; i < V; i++) {
-	        if(!vis[i]) {
-	            dfs(i, vis, s, adj);
-	        }
-	    }
-	    vector<int> res;
-	    for(int i = 0; i < V; i++) {
-	        res.push_back(s.top()); s.pop();
-	    }
-	    return res;
-	}
-};
+//         for(int i=0; i<V; i++){
+//             adj.add(new ArrayList<>());
+//         }
+//         for(int i=0; i<edges.length; i++){
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+//             adj.get(u).add(v);
+//         }
+//         boolean [] visited = new boolean [V];
+//         Stack<Integer> st = new Stack<>();
+//         for(int i=0; i<V; i++){
+//             if(!visited[i]){
+//                 dfs(i, visited, adj, st);
+//             }
+//         }
+        
+//         while(! st.isEmpty()){
+//             int val = st.pop();
+//             res.add(val);
+//         }
+//         return res;
+//     }
