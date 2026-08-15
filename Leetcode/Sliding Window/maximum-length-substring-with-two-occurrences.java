24fc9b4d84c3846f05ecbc044dd45b47231d33e1
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-08-15 18:16:11 +0530

    Solved Maximum Length Substring With Two Occurrences

---
+++ Leetcode/Sliding Window/maximum-length-substring-with-two-occurrences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int n = s.length();
+        int left = 0;
+        HashMap<Character, Integer> freq = new HashMap<>();
+        int maxLen = 0;
+        for(int right=0; right<n; right++){
+            char ch = s.charAt(right);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+            while(freq.get(ch) > 2){
+                char leftChar = s.charAt(left);
+                freq.put(leftChar, freq.get(leftChar) - 1);
+                left++; 
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}
