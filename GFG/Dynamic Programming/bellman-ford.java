079f8e73c54f5be7bd2b3ffa5af0368aac74cf43
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-08-22 20:21:21 +0530

    Solved Bellman Ford

---
+++ GFG/Dynamic Programming/bellman-ford.java
@@ -0,0 +1,41 @@
+class Solution {
+    public ArrayList<Integer> bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int [] distance = new int [V];
+        Arrays.fill(distance, (int)1e8);
+        distance[src] = 0;
+            
+        for(int i=0; i<V; i++){
+            for(int [] edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(distance[u] != 1e8 && w + distance[u] < distance[v]){
+                    distance[v] = w + distance[u];
+                }
+            }
+        }
+        
+        for(int i=0; i<V; i++){
+           for(int [] edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                
+                if(distance[u] != 1e8 && w + distance[u] < distance[v]){
+                    ArrayList<Integer> cycle = new ArrayList<>();
+                    cycle.add(-1);
+                    return cycle;
+                }
+            }
+        }
+        
+        for(int dist : distance){
+            res.add(dist);
+        }
+        return res;
+    }
+}
