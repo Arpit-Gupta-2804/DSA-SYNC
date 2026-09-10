57a856a5e906c9922df219cc4896e4ecc58f7ab9
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-09-10 12:56:42 +0530

    Solved Frog Jump

--- GFG/Dynamic Programming/frog-jump.java
+++ GFG/Dynamic Programming/frog-jump.java
@@ -1,19 +1,19 @@
 class Solution {
     int minCost(int[] height) {
         // code here
-        int [] dp = new int [height.length + 1];
-        Arrays.fill(dp, -1);
-        return helper(height, height.length - 1, dp);
-    }
-    private int helper(int [] height, int n, int [] dp){
-        if(n == 0) return 0;
+        int prev = 0;
+        int curr = 0;
         
-        if(dp[n] != -1) return dp[n];
-        
-        int left = helper(height, n-1, dp) + Math.abs(height[n] - height[n-1]);
-        int right = Integer.MAX_VALUE;
-        if(n > 1) right = helper(height, n-2, dp) + Math.abs(height[n] - height[n-2]);
+        for(int i=1; i<height.length; i++){
+            int left = curr + Math.abs(height[i] -  height[i-1]);
             
-        return dp[n] = Math.min(left, right);
+            int right = Integer.MAX_VALUE;
+            if(i > 1)    
+                right = prev + Math.abs(height[i] - height[i-2]);
+            int next = Math.min(right, left);
+            prev = curr;
+            curr = next;
+        }
+        return curr;
     }
 }
