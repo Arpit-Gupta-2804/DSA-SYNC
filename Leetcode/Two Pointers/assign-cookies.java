92283b88430e47e92c000167cd569f89b0704506
Arpit Gupta <arpitg.mca24@nitp.ac.in>
2026-09-13 10:22:03 +0530

    Solved Assign Cookies

---
+++ Leetcode/Two Pointers/assign-cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int left = 0;
+        int right = 0;
+
+        while(left < m && right < n){
+            if(s[left] >= g[right]){
+                left++;
+                right++;
+            }else{
+                left++;
+            }
+        }
+        return right;
+    }
+}
